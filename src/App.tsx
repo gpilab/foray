e8a78d6b7ee5dca95f5b500fb127dc7032ca31a9
John Christensen <johnedchristensen@gmail.com>
2024-04-25 16:28:16 -0500

    Add custom Latex render tool. Proof of concept

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,30 @@
 import { TLShape, Tldraw } from 'tldraw'
 import CustomUi from './custom-ui'
-
+import { CardShapeUtil } from './tools/card/CardShapeUtil'
+import { CardShapeTool } from './tools/card/CardShapeTool'
+import { components, uiOverrides, } from './tools/card/ui-overrides'
 import 'tldraw/tldraw.css'
 
 // aquired via "copy as JSON", which is an option when debug
 // mode is on
 import _startShape from './assets/init_snapshot.json'
-const startShapes = _startShape.shapes as Array<TLShape>
+
 
 export default function CustomUiExample() {
   return (
     <Tldraw
+      shapeUtils={[CardShapeUtil]}
+      tools={[CardShapeTool]}
+      overrides={uiOverrides}
+      components={components}
+
       inferDarkMode
       persistenceKey='gpi_v2'
       onMount={(editor) => {
         editor.updateInstanceState({ isFocusMode: true })
-        editor.createShapes(startShapes)
+        editor.createShapes((_startShape.shapes as Array<TLShape>))
       }}>
       <CustomUi />
     </Tldraw>
   )
 }
-
