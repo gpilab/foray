5ed6c96f8f9bd16a6b7e7496cbbeac2e5273ac9d
John Christensen <jechristens3@john-macbook.local>
2024-05-17 15:41:35 -0500

    Cleanup partial implementations

--- src/graph/node.ts
+++ src/graph/node.ts
@@ -1,132 +1,65 @@
-
-type FlowType = number | number[]
-
-type Port<T> = {
-  //id: string
-  dataType?: T
+type CustomType = {
+  a: string
+  b: boolean
 }
 
-type GPI_Node<InputTypes, OutputTypes> = {
+// All possible data types that can be passed through ports
+interface PortDataType {
+  number: number
+  vec: number[]
+  string: string
+  customType: CustomType
+}
+// the interface can get updated over time, or by API consumers?
+interface PortDataType {
+  coolType: boolean
+}
+// An actual instance of data that can be on a port 
+// The keys of PortDataType are removed, leaving just a union of the data type
+type PortDataInstance<T extends keyof PortDataType> = PortDataType[T]
+
+
+interface inputPort<T extends keyof PortDataType> {
+  ioType: "in"
   label: string
-  id: string
-  inputs: { [K in keyof InputTypes]: Port<InputTypes[K]> }
-  outputs: { [K in keyof OutputTypes]: Port<OutputTypes[K]> }
-  compute: (inputs: InputTypes) => OutputTypes
+  dataType: T
+  heldValue?: PortDataInstance<T>
 }
-
-type constIn = {}
-type constOut<T> = {
-  a: T
-}
-
-type BinaryIn<T> = {
-  a: T
-  b: T
+interface outputPort<T extends keyof PortDataType> {
+  ioType: "out"
+  label: string
+  dataType: T
+  heldValue?: PortDataInstance<T>
 }
 
 
-const add: GPI_Node<BinaryIn<number>, number> = {
-  label: "add",
-  id: "add1",
-  inputs: {
-    a: { portName: "number" }, b: { dataType: "number" }
-  },
-  compute: (inputs) => {
-    console.log("add", inputs.a, inputs.b);
-    return inputs.a + inputs.b
-  }
-
+function createInput<T extends keyof PortDataType>(label: string, dataType: T): inputPort<T> {
+  return { ioType: "in", label: label, dataType: dataType }
 }
 
-const constNode7: GPI_Node = {
-  label: "constant 7",
-  id: "const7",
-  inputs: [],
-  outputs: [{ id: "a" }],
-  compute: (a, b) => 7
-}
-const constNode4: GPI_Node = {
-  label: "constant 4",
-  id: "const4",
-  inputs: [],
-  outputs: [{ id: "a" }],
-  compute: (a, b) => 4
+function createOutput<T extends keyof PortDataType>(label: string, dataType: T): outputPort<T> {
+  return { ioType: "out", label: label, dataType: dataType }
 }
 
-
-
-const subtract: GPI_Node = {
-  label: "subtract",
-  id: "add2",
-  inputs:
-    outputs: [{ id: "c" }],
-  compute: (inputs) => {
-    console.log("subtract", inputs.a, inputs.b);
-    return a - b
-  }
+function isCompatiblePort(output: outputPort<any>, input: inputPort<any>) {
+  return output.dataType === input.dataType
 }
 
-type Graph = {
-  nodes: GPI_Node[],
-  wires: Wire[]
-}
+const testOutPortA = createOutput("myLabelOut", "number")
+const testInPortA = createInput("myLabelIn", "number")
+const testOutPortC = createOutput("myCustomOut", "vec")
+const testOutPortD = createOutput("coolTypeLabel", "coolType")
 
-const g: Graph = {
-  nodes: [constNode7, constNode4, add],
-  wires: [{ startNode: constNode7, startPortId: "a", endNode: add, endPortId: "a" }
-    , { startNode: constNode4, startPortId: "a", endNode: add, endPortId: "b" }]
-}
+testInPortA.heldValue = 1
+testInPortA.heldValue = 2
+testOutPortC.heldValue = [1, 2, 3]
 
+console.log(testOutPortA, testOutPortA.dataType)
+console.log(testInPortA)
+console.log(isCompatiblePort(testOutPortA, testInPortA))
 
-function getParentNodes(node: GPI_Node, wires: Wire[]) {
-  let parents: GPI_Node[] = []
-  node.inputs.forEach((input) => {
-    let wire = wires.find((w) => (w.endNode == node && w.endPortId == input.id))
-    if (wire) {
-      parents.push(wire.startNode)
-    }
-  })
-  return parents
-}
-
-function evalNode(node: GPI_Node, processedNodes: GPI_Node[], wires: Wire[]) {
-  if (node.inputs.length == 0) {
-    console.log("reached leaf!", node)
-    processedNodes.push(node)
-    return node.compute(0, 0)
-  }
-  const parents = getParentNodes(node, wires)
-  parents.forEach((p) => evalNode(p, processedNodes, wires))
-
-  processedNodes.push(node)
-  return node.compute(node.inputs)
-
-}
-
-function evalGraph(g: Graph, startNodeIndex: number) {
-  let currentNode = g.nodes[startNodeIndex]
-  let processedNodes = []
-
-  while (processedNodes.length < g.nodes.length) {
-    const inputs = currentNode.inputs
-    const inputValues = new Array<number>
-    inputs.forEach((input) => {
-      let wire = g.wires.find((w) => (w.endNode == currentNode && w.endPortId == input.id))
-      if (wire) {
-        const valueForInput = wire.startNode.compute(0, 0)
-        inputValues.push(valueForInput)
-        processedNodes.push(wire.startNode)
-      }
-    })
-    const value = currentNode.compute(inputValues[0], inputValues[1])
-    console.log(value)
-    processedNodes.push(currentNode)
-
-  }
-
-}
-
-evalGraph(g, 2)
-
+console.log(testOutPortC)
+console.log(testInPortA)
+console.log(isCompatiblePort(testOutPortC, testInPortA))
 
 
