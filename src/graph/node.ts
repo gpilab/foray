01eda6b2f46e408848dd0124231e0bddd3afa766
John Christensen <johnedchristensen@gmail.com>
2024-05-21 14:43:44 -0500

    Implement node creation w/ only a function input The function is constrained to use appropriate inputs and outputs

--- src/graph/node.ts
+++ src/graph/node.ts
@@ -1,5 +1,3 @@
-
-
 type CustomType = {
   a: string
   b: boolean
@@ -15,11 +13,18 @@ export interface PortDataTypes {
   customType: CustomType
 }
 
+export type PortDataTypesTuple = [
+  number,
+  number[],
+  string,
+  CustomType,
+]
+
 
 // An actual instance of data that can be on a port 
 // The keys of PortDataType are removed, leaving just a union of the data type
 // not currently needed
-//type PortDataInstance<T extends keyof PortDataTypes> = PortDataTypes[T]
+type PortDataInstance = PortDataTypes[keyof PortDataTypes]
 
 /** Common values that input and output ports share
  */
@@ -31,61 +36,119 @@ interface BasePort<T extends keyof PortDataTypes> {
 
 /** Defines the shape of values that a  node can accept as input
  */
-interface inputPort<T extends keyof PortDataTypes> extends BasePort<T> {
+interface inputPort
+  <T extends keyof PortDataTypes> extends BasePort<T> {
   ioType: "in"
 }
 
 /** Defines the shape of values that a the a node can produce
  */
-interface outputPort<T extends keyof PortDataTypes> extends BasePort<T> {
+interface outputPort
+  <T extends keyof PortDataTypes> extends BasePort<T> {
   ioType: "out"
 }
 
 
-export function createInput<T extends keyof PortDataTypes>(label: string, dataType: T): inputPort<T> {
+export function createInput
+  <T extends keyof PortDataTypes>
+  (label: string, dataType: T): inputPort<T> {
   return { ioType: "in", label: label, dataType: dataType }
 }
 
-export function createOutput<T extends keyof PortDataTypes>(label: string, dataType: T): outputPort<T> {
+export function createOutput
+  <T extends keyof PortDataTypes>
+  (label: string, dataType: T): outputPort<T> {
   return { ioType: "out", label: label, dataType: dataType }
 }
 
 /** Tests if two ports can be wired together
   */
-export function isCompatiblePort(output: outputPort<any>, input: inputPort<any>) {
+export function isCompatiblePort
+  (output: outputPort<any>, input: inputPort<any>) {
   //TODO: account for cyclic graphs/self assignment (those might be same thing?)
   return output.dataType === input.dataType
 }
 
-const testOutPortA = createOutput("myLabelOut", "number")
-const testInPortA = createInput("myLabelIn", "number")
-const testOutPortC = createOutput("myCustomOut", "vec")
-
-testInPortA.heldValue = 1
-testInPortA.heldValue = 2
-testOutPortC.heldValue = [1, 2, 3]
-
-console.log(testOutPortA, testOutPortA.dataType)
-console.log(testInPortA)
-console.log(isCompatiblePort(testOutPortA, testInPortA))
-
-console.log(testOutPortC)
-console.log(testInPortA)
-console.log(isCompatiblePort(testOutPortC, testInPortA))
 
 
-export interface BaseNode<T extends inputPort<any>[], U extends outputPort<any>[]> {
+
+export interface BaseNode {
   label: string
-  id: string
-  inputPorts: T
-  outputPorts: U
-  compute: (inputs: T) => U
+  // inputPorts: Parameters<T>
+  // outputPorts: ReturnType<T>
+  compute: nodeCompute
+}
+
+function multiply(...l: number[]) {
+  return l.reduce((p, c) => (p * c))
+}
+console.log(multiply(1, 2, 3))
+
+//type addable = number | number[] | string
+
+// type myType = string | number | number[]
+// type myCompute<T extends myType, U extends myType> = (...a: T[]) => U
+//
+// const a: myCompute<number, number> = (a: number, b: number) => {
+//   return a + b
+// }
+//
+// type myCompute2 = (...a: (string | number)[]) => string | number
+//
+// const a2: myCompute2 = (a: number, b: number) => {
+//   return a + b
+// }
+
+
+type NodeInOut = string | number | number[] | boolean
+
+type ComputeInput = {} | { [key: string]: NodeInOut }
+type ComputeOutput = NodeInOut | { [key: string]: NodeInOut }
+
+type Compute<Input extends ComputeInput, Output extends ComputeOutput> = (input: Input) => Output
+
+type add_input = {
+  a: number,
+  b: number
+}
+
+type NodeTemplate<Input extends ComputeInput, Output extends ComputeOutput> = {
+  label: string
+  compute: Compute<Input, Output>
+}
+
+function createNode<Input extends ComputeInput, Output extends NodeInOut>(label: string, f: (i: Input) => Output): NodeTemplate<Input, Output> {
+  return {
+    label: label,
+    compute: f
+  }
 }
 
 
-export interface addNode<T extends [inputPort<"number">, inputPort<"number">], U extends [sum: outputPort<"number">]> extends BaseNode<T, U> {
-  label: "add"
-  compute: (inputs: T) => U
+const a3 = ({ a, b }: add_input) => {
+  return a + b
+}
+
+
+const add_node: NodeTemplate<add_input, number> = { label: "add", compute: a3 }
+
+const not = createNode("not", (a: boolean) => { return !a })
+
+
+const a4 = () => {
+  return false
+}
+
+const falseNode: NodeTemplate<Parameters<typeof a4>, ReturnType<typeof a4>> = { label: "false", compute: a4 }
+const falseNode2 = createNode("false", a4)
+
+
+
+
+
+// old implementation
+export interface addNode extends BaseNode {
+  compute: typeof a3
 }
 
 const addInA = createInput("a", "number")
@@ -96,7 +159,6 @@ const addOutSum = createOutput("sum", "number")
 
 const addNodeInstance: addNode<[inputPort<"number">, inputPort<"number">], [outputPort<"number">]> = {
   label: "add",
-  id: "abc123",
   inputPorts: [addInA, addInB],
   outputPorts: [addOutSum],
   compute: (inputs: [inputPort<"number">, inputPort<"number">]) => {
--- src/graph/nodes_test.ts
+++ src/graph/nodes_test.ts
@@ -1,10 +1,26 @@
-import { createOutput, createInput } from './node'
+import { createOutput, createInput, isCompatiblePort } from './node'
+
+const testOutPortA = createOutput("myLabelOut", "number")
+const testInPortA = createInput("myLabelIn", "number")
+const testOutPortC = createOutput("myCustomOut", "vec")
+
+testInPortA.heldValue = 1
+testInPortA.heldValue = 2
+testOutPortC.heldValue = [1, 2, 3]
+
+console.log(testOutPortA, testOutPortA.dataType)
+console.log(testInPortA)
+console.log(isCompatiblePort(testOutPortA, testInPortA))
+
+console.log(testOutPortC)
+console.log(testInPortA)
+console.log(isCompatiblePort(testOutPortC, testInPortA))
+
+//TODO setup jest testing
 
 // the interface can get updated over time, or by API consumers?
 // interface PortDataTypes {
 //   coolType: boolean
 // }
-const out1 = createOutput("out1", "number")
-const in1 = createInput("in1", "number")
 
-////
+
