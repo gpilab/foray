2d75de9a2c4920ddc78f9480e1307395a8609641
John Christensen <johnedchristensen@gmail.com>
2024-05-28 11:09:59 -0500

    Basic working Signals

--- src/graph/node_simplify.ts
+++ src/graph/node_simplify.ts
@@ -5,38 +5,45 @@
 
 type DataType = 'number' | 'boolean';
 
+interface PortConnection {
+  toNodeId: string,
+  toPortId: string
+}
+
 interface BaseIO {
-  label: string;
   type: DataType;
-  value?: any; // Optional value for computation
-  ioType?: "in" | "out"
 }
 
 interface Input extends BaseIO {
-  ioType?: "in"
+  value?: any;
 }
 
 interface Output extends BaseIO {
-  ioType?: "out"
+  value?: any;
+  portConnection?: PortConnection
 }
 
+// key is the label for the port
+type Ports<T extends BaseIO> = Record<string, T>
 
-interface PortConnection {
-  toNodeId: string
-  toPortId: string
-}
-type PortConnectionMap = Record<string, PortConnection>
+// interface Input extends BaseIO {
+//   ioType?: "in"
+// }
+//
+// interface Output extends BaseIO {
+//   ioType?: "out"
+// }
 
 
 
-interface FunctionNodeBase<I extends BaseIO[], O extends BaseIO[]> {
-  id: string;
-  type: string;
-  inputs: I;
-  outputs: O;
-  inputConnections: PortConnectionMap;
-  outputConnections: PortConnectionMap;
-  compute: (...args: any[]) => any; // Function to compute outputs from inputs
+
+
+interface FunctionNodeBase<I extends Ports<Input>, O extends Ports<Output>> {
+  id: string
+  type: string
+  inputs: I
+  outputs: O
+  compute: (...args: any[]) => any
 }
 
 
@@ -45,142 +52,133 @@ interface FunctionNodeBase<I extends BaseIO[], O extends BaseIO[]> {
 
 
 interface AddFunction extends FunctionNodeBase<
-  [{ label: 'a', type: 'number' }, { label: 'b', type: 'number' }],
-  [{ label: 'sum', type: 'number', value?: number }]
-> {
-  type: 'add';
+  { a: { type: 'number', value?: number }, b: { type: 'number', value?: number } },
+  { sum: { type: 'number', value?: number } }> {
+  type: 'add'
+  inputs: { a: { type: 'number', value?: number }, b: { type: 'number', value?: number } },
+  outputs: { sum: { type: 'number', value?: number } },
   compute: (a: number, b: number) => number;
 }
-
-interface NotFunction extends FunctionNodeBase<
-  [{ label: 'a', type: 'boolean' }],
-  [{ label: 'not_a', type: 'boolean', value?: number }]
-> {
-  type: 'not';
-  compute: (a: boolean) => boolean;
-}
-
-interface ConstantNumberFunction extends FunctionNodeBase<
-  [],
-  [{ label: 'c', type: 'number', value?: number }]
-> {
-  type: 'constant_number';
-  compute: () => number;
-}
-
-type FunctionNode = AddFunction | NotFunction | ConstantNumberFunction;
-
-
-// function Factories
-
 
 const createAddFunction = (id: string): AddFunction => ({
   id,
   type: 'add',
-  inputs: [{ label: 'a', type: 'number' }, { label: 'b', type: 'number' }],
-  outputs: [{ label: 'sum', type: 'number' }],
-  inputConnections: {},
-  outputConnections: {},
+  inputs: { a: { type: 'number' }, b: { type: 'number' } },
+  outputs: { sum: { type: 'number' } },
   compute: (a: number, b: number) => a + b
 });
+
+
+interface NotFunction extends FunctionNodeBase<
+  { a: { type: 'boolean', value?: boolean } },
+  { not_a: { type: 'boolean', value?: boolean } }> {
+  type: 'not'
+  inputs: { a: { type: 'boolean' } },
+  outputs: { not_a: { type: 'boolean' } },
+  compute: (a: boolean) => boolean;
+}
 
 const createNotFunction = (id: string): NotFunction => ({
   id,
   type: 'not',
-  inputs: [{ label: 'a', type: 'boolean' }],
-  outputs: [{ label: 'not_a', type: 'boolean' }],
-  inputConnections: {},
-  outputConnections: {},
+  inputs: { a: { type: 'boolean' } },
+  outputs: { not_a: { type: 'boolean' } },
   compute: (a: boolean) => !a
 });
+
+
+interface ConstantNumberFunction extends FunctionNodeBase<
+  {},
+  { c: { type: 'number', value: number } }> {
+  type: 'constant_number'
+  inputs: {},
+  outputs: { c: { type: 'number', value: number } },
+  compute: () => number
+}
 
 const createConstantNumberFunction = (id: string, constant: number): ConstantNumberFunction => ({
   id,
   type: 'constant_number',
-  inputs: [],
-  outputs: [{ label: 'c', type: 'number', value: constant }],
-  inputConnections: {},
-  outputConnections: {},
+  inputs: {},
+  outputs: { c: { type: 'number', value: constant } },
   compute: () => constant
 });
 
 
+type FunctionNode = AddFunction | NotFunction | ConstantNumberFunction;
+
+// Node Utilities
+
 const connectNodes = <
-  ON extends FunctionNodeBase<any, any>,
-  IN extends FunctionNodeBase<any, any>,
-  OL extends ON['outputs'][number]['label'],
-  IL extends IN['inputs'][number]['label'],
+  ON extends FunctionNode,
+  IN extends FunctionNode,
+  OL extends keyof ON['outputs'],
+  IL extends keyof IN['inputs']
 >(
   outputNode: ON,
   outputLabel: OL,
   inputNode: IN,
   inputLabel: IL
 ): void => {
-  console.log("in connect")
-  console.log(`Connecting output ${outputNode.id}, label: ${outputLabel},\nto ${inputNode.id}, label: ${inputLabel}`)
-  const output = outputNode.outputs.find((o: Output) => o.label === outputLabel);
-  const input = inputNode.inputs.find((i: Input) => i.label === inputLabel);
-
-  if (!output || !input) {
-    throw new Error(`Label not found on node. This should never happen. ${outputLabel}, ${inputLabel}`);
-  }
-
-  if (output.type !== input.type) {
-    throw new Error(`Type mismatch: cannot connect ${output.type} to ${input.type}`);
-  }
-
-  // Add connections
-  // if (!outputNode.outputConnections.find(o => o.label == outputLabel)) {
-  outputNode.outputConnections[outputLabel] = {
+  console.log(outputNode.outputs)
+  outputNode.outputs[outputLabel].portConnection = {
     toNodeId: inputNode.id,
     toPortId: inputLabel
   }
-  // }
-  //outputNode.outputConnections[outputLabel]?.push(input);
-
-  inputNode.inputConnections[inputLabel] = {
-    toNodeId: outputNode.id,
-    toPortId: outputLabel
-  }
-  // inputNode.inputConnections[inputLabel] = output;
-  // input.value = output.value; // Propagate initial value
+  console.log(outputNode.outputs)
 };
 
-function fill_inputs(node: FunctionNode, all_nodes: FunctionNode[]) {
+const setInputValue = (nodeId: string, portLabel: string, value: any, all_nodes: FunctionNode[]) => {
+  const node = all_nodes.find(n => n.id == nodeId)
+  if (!node) {
+    throw Error(`Could not find nodeId:${nodeId}`)
+  }
+  const input = node.inputs[portLabel]
 
-  Object.entries(node.inputConnections).map(([label, portConnection]) => {
-    console.log("Port Connection", portConnection.toNodeId)
-    const portLabel = portConnection.toPortId
-    const connectedNode = all_nodes.find(n => n.id == portConnection.toNodeId) as FunctionNode
-
-    const output = connectedNode.outputs.find(o => o.label == portLabel) as Output
-    console.log(`grabbing value for ${node.id}, label: ${label}\n This node: ${node} \n looking for value from node ${connectedNode.id}, label ${portLabel}`)
-    console.log(output)
-    node.inputs.find(i => i.label == label).value = output.value
-  })
+  if (!input) {
+    throw Error(`Could not find portLabel,${portLabel}, on the inputs of node: ${nodeId}`)
+  }
+  input.value = value
 }
 
 const runCompute = (node: FunctionNode, all_nodes: FunctionNode[]): void => {
-  fill_inputs(node, all_nodes)
-  const inputs = node.inputs.map((input, label) => { label: input.value })
-  const outputValues = node.compute(...inputs)
+  const computeInputs = Object.entries(node.inputs).map<Input>(([_label, input]) => input.value)
+  console.log(computeInputs)
+  const outputValues = node.compute(...computeInputs)
+  console.log(outputValues)
 
-  node.outputs.forEach((output, index) => {
-    output.value = outputValues[index];
+
+  Object.entries(node.outputs).forEach(([_index, o]) => {
+    console.log(_index, o)
+    const output = o as Output
+    output.value = outputValues
+    if (output.portConnection) {
+      setInputValue(output.portConnection.toNodeId, output.portConnection.toPortId, outputValues, all_nodes)
+    }
   });
 };
+
 // Usage
 
 
-const addNode = createAddFunction('add1');
+const add_Node = createAddFunction('add1');
+const add_Node2 = createAddFunction('add2');
+const add_Node3 = createAddFunction('add3');
 const notNode = createNotFunction('not1');
 const constantNumberNode = createConstantNumberFunction('const_num1', 5);
 const constantNumberNode2 = createConstantNumberFunction('const_num2', 7);
-const all_nodes = [addNode, constantNumberNode, constantNumberNode2]
+const constantNumberNode3 = createConstantNumberFunction('const_num1', 13);
+const constantNumberNode4 = createConstantNumberFunction('const_num2', 17);
+const all_nodes = [add_Node, add_Node2, add_Node3, notNode, constantNumberNode, constantNumberNode2, constantNumberNode3, constantNumberNode4]
 // Connect the nodes
 console.log("before connect")
-connectNodes(constantNumberNode, 'c', addNode, 'a'); // Connect constant number output to add input 'a'
-connectNodes(constantNumberNode2, 'c', addNode, 'b'); // Connect constant number output to add input 'a'
+connectNodes(constantNumberNode, 'c', add_Node, 'a');
+connectNodes(constantNumberNode2, 'c', add_Node, 'b');
+connectNodes(constantNumberNode3, 'c', add_Node2, 'a');
+connectNodes(constantNumberNode4, 'c', add_Node2, 'b');
+
+connectNodes(add_Node, 'sum', add_Node3, 'a');
+connectNodes(add_Node2, 'sum', add_Node3, 'b');
 
 // Set a value for the second input of the add node
 //addNode.inputs.find(input => input.label === 'b')!.value = 10;
@@ -189,7 +187,15 @@ connectNodes(constantNumberNode2, 'c', addNode, 'b'); // Connect constant number
 console.log("running constant")
 runCompute(constantNumberNode, all_nodes);
 runCompute(constantNumberNode2, all_nodes);
+runCompute(constantNumberNode3, all_nodes);
+runCompute(constantNumberNode4, all_nodes);
 console.log("running add")
-runCompute(addNode, all_nodes);
-
-console.log(addNode.outputs.find(output => output.label === 'sum')!.value); // Should output 15
+runCompute(add_Node, all_nodes);
+runCompute(add_Node2, all_nodes);
+runCompute(add_Node3, all_nodes);
+console.log(constantNumberNode)
+console.log(constantNumberNode2)
+console.log(add_Node)
+console.log(add_Node.outputs["sum"].value);
+console.log(add_Node2.outputs["sum"].value);
+console.log(add_Node3.outputs["sum"].value); 
--- src/graph/rxjs_hello_world.ts
+++ src/graph/rxjs_hello_world.ts
@@ -1,75 +1,88 @@
-import { Observable, Subject, combineLatest } from 'rxjs';
+import { Observable, ReplaySubject, combineLatest } from 'rxjs';
 import { map } from 'rxjs/operators';
 
-class Node {
-  public inputSubjects: Subject<any>[] = [];
-  public output$: Observable<any>;
-  public transformationFunction: Function;
 
-  constructor(transformationFunction: Function, numberOfInputs: number) {
+
+// Define a generic Node class with typed inputs based on the transformation function
+class Node<TFunction extends (...args: any[]) => any> {
+  public inputSubjects: ReplaySubject<any>[];
+  public output$: Observable<ReturnType<TFunction>>;
+  private transformationFunction: TFunction;
+
+  constructor(transformationFunction: TFunction) {
     this.transformationFunction = transformationFunction;
-    for (let i = 0; i < numberOfInputs; i++) {
-      this.inputSubjects.push(new Subject<any>());
-    }
+    const numInputs = transformationFunction.length;
+    this.inputSubjects = Array.from({ length: numInputs }, () => new ReplaySubject<any>(1));
 
-    // Combine all inputs into a single observable if more than one input is expected
-    if (numberOfInputs > 1) {
-      this.output$ = combineLatest(this.inputSubjects).pipe(
-        map(inputs => this.transformationFunction(...inputs))
-      );
-    } else {
-      this.output$ = this.inputSubjects[0].pipe(
-        map(input => this.transformationFunction(input))
-      );
-    }
-  }
-
-  emit(inputIndex: number, value: any) {
-    if (inputIndex < this.inputSubjects.length) {
-      this.inputSubjects[inputIndex].next(value);
-    }
+    this.output$ = combineLatest(this.inputSubjects).pipe(
+      map(inputs => {
+        const value = this.transformationFunction(...inputs as Parameters<TFunction>);
+        console.log(`Processing (${inputs}) through ${transformationFunction}....${value}`);
+        return value;
+      }));
   }
 }
 
 class Graph {
-  private nodes: Node[] = [];
+  private nodeAdjacencies: Map<Node<any>, Node<any>[]> = new Map();
 
-  addNode(node: Node) {
-    this.nodes.push(node);
+  addNode(node: Node<any>, connections: { targetNode: Node<any>, targetInputIndex: number }[] = []) {
+    this.nodeAdjacencies.set(node, []);
+    connections.forEach(({ targetNode, targetInputIndex }) => {
+      this.connectNodes(node, targetNode, targetInputIndex);
+    });
+  }
 
-    // If there's a previous node, connect its output to the input of the new node
-    if (this.nodes.length > 1) {
-      const previousNode = this.nodes[this.nodes.length - 2];
-      previousNode.output$.subscribe({
-        next: output => {
-          // If the new node has multiple inputs, this needs adjustment
-          node.inputSubjects[0].next(output);  // Assumes single-input scenario for simplicity
-        }
-      });
+  // Connect output of one node to input of another node
+  connectNodes(sourceNode: Node<any>, targetNode: Node<any>, targetInputIndex: number = 0) {
+    const sourceNodeAdjacencies = this.nodeAdjacencies.get(sourceNode)
+    if (sourceNodeAdjacencies == undefined) {
+      console.error("Source node not present in graph");
+      return;
     }
+    if (!this.nodeAdjacencies.has(targetNode)) {
+      console.error("Target node not present in graph");
+      return;
+    }
+    // Add target node to the adjacency list of the source node
+    sourceNodeAdjacencies.push(targetNode);
+
+    // Subscribe the output of the source node to the input of the target node
+    sourceNode.output$.subscribe(output => {
+      targetNode.inputSubjects[targetInputIndex].next(output);
+    });
   }
 }
 
-function double(x: number): number {
-  return x * 2;
-}
-
-function add(x: number, y: number): number {
-  console.log("adding nums", x, y)
-  return x + y;
-}
-
 const graph = new Graph();
-const doubleNode = new Node(double, 1);
+
+const constantNode = new Node((x) => x);
+const doubleNode = new Node((x: number) => x * 2);
+const addNode = new Node((x, y) => x + y);
+const subtractNode = new Node((x, y) => x - y);
+// Subscribe to outputs to see the typed results
+subtractNode.output$.subscribe(result => console.log("Result from subtractNode:", result));
+
+graph.addNode(constantNode);
 graph.addNode(doubleNode);
-
-// Assumes addNode expects two inputs; second input needs to be handled separately
-const addNode = new Node(add, 2);
 graph.addNode(addNode);
-console.log(addNode.inputSubjects)
+graph.addNode(subtractNode);
 
-doubleNode.emit(0, 5);  // Output should be 10, expected to be input to addNode
-addNode.emit(1, 3);     // Combined with 10, should output 13
-addNode.emit(0, 3);     // Combined with 10, should output 13
+// Explicitly connect nodes
+graph.connectNodes(constantNode, doubleNode, 0);
+graph.connectNodes(constantNode, addNode, 1);
+graph.connectNodes(doubleNode, addNode, 0);
+graph.connectNodes(doubleNode, subtractNode, 0);
+graph.connectNodes(addNode, subtractNode, 1);
 
-addNode.output$.subscribe(result => console.log("Result from addNode:", result));
+
+
+constantNode.inputSubjects[0].next(5)
+
+// const sin: Observable<number> = timer(0, 10) // Emit values every 100 ms
+//   .pipe(
+//     map((t: number) => Math.sin(t / 1000 * Math.PI * 2) * 0.5 + 0.5), // t / 1000 to slow down the frequency for better visualization
+//     throttleTime(1000)
+//   );
+// sin.subscribe(constantNode.inputSubjects[0])
+//
