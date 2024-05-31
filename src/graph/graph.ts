1f31e3780b3030dd66e1f09106d4d78362130c61
John Christensen <johnedchristensen@gmail.com>
2024-05-31 14:34:10 -0500

    Add additional node tests

--- src/graph/graph.ts
+++ src/graph/graph.ts
@@ -16,34 +16,96 @@ type PortTypeKeys = keyof ValidPortTypes
 // type PortTypes = ValidPortTypes[keyof ValidPortTypes]
 
 /** Node inputs are described by a unique label and a data type. Data types are defined as the keys of ValidPortTypes */
-type InPort = [string, PortTypeKeys]
-type NodeInputs = InPort[]
+type InPort = readonly [string, PortTypeKeys]
+type NodeInputs = readonly InPort[]
 
 /** Compute outputs based on inputs */
 // type ComputeInputs<T extends NodeInputs> = { [k in keyof T]: ValidPortTypes[T[k][1]] }
 
 
-type InputDataTypes<T extends NodeInputs> = {
-  [K in keyof T]: T[K] extends [string, infer U] ? (U extends PortTypeKeys ? ValidPortTypes[U] : never) : never;
+type InputTypes<T extends NodeInputs> = {
+  [K in keyof T]: T[K] extends readonly [string, infer U] ? (U extends PortTypeKeys ? ValidPortTypes[U] : never) : never;
+};
+type InputTypeLabels<T extends NodeInputs> = {
+  [K in keyof T]: T[K] extends readonly [string, infer U] ? (U extends PortTypeKeys ? U : never) : never;
 };
 
+type InputKeys<T extends NodeInputs> = {
+  [K in keyof T]: T[K] extends readonly [infer U, PortTypeKeys] ? (U extends string ? U : never) : never;
+};
+
+// type InputTypesUnion<T extends NodeInputs> = InputTypes<T>[number]
+type InputTypeLabelsUnion<T extends NodeInputs> = InputTypeLabels<T>[number]
+type InputKeysUnion<T extends NodeInputs> = InputKeys<T>[number]
+
+// const n = [["x", "number"], ["s", "string"]] as const
+//
+// type N = typeof n
+//
+// type NT = InputTypes<N>
+// let nt: NT
+// nt = [1, "a"]
+//
+// type NTU = InputTypesUnion<N> // string | number
+// let ntu: NTU
+// ntu = 1
+// ntu = "a"
+// //@ts-expect-error
+// ntu = { a: 1 }
+//
+// type NTL = InputTypeLabels<N> // ["number", "string"]
+// let ntl: NTL
+// ntl = ["number", "string"]
+// //@ts-expect-error
+// ntl = ["string", "number"]
+// //@ts-expect-error
+// ntl = ["number"]
+//
+// type NTLU = InputTypeLabelsUnion<N> //  "string" | "number"
+// let ntlu: NTLU
+// ntlu = "number"
+// ntlu = "string"
+// //@ts-expect-error
+// ntlu = "boolean"
+//
+// type NL = InputKeys<N> // ["x","s"]
+// let nl: NL
+// nl = ["x", "s"]
+// //@ts-expect-error
+// nl = ["s", "x"]
+//
+// type NLU = InputKeysUnion<N> // "x" | "s"
+// let nlu: NLU
+// nlu = "s"
+// nlu = "x"
+// //@ts-expect-error
+// nlu = "z"
+//
+type InputTypeByLabel<T extends NodeInputs, K extends string> = Extract<T[number], readonly [K, any]>[1];
+//
+// let xType: InputTypeByLabel<N, "x">
+// xType = "number"
+// let yType: InputTypeByLabel<N, "s">
+// yType = "string"
 
 export class Node<I extends NodeInputs = any, O extends PortTypeKeys = any> {
   public id: string
   public inputStreams: Map<string, ReplaySubject<ValidPortTypes[PortTypeKeys]>>
   public outputStream$: Observable<ValidPortTypes[O]>
-  public inputTypes: Map<string, PortTypeKeys>
+  readonly inputMap: I
+  public inputTypes: Map<InputKeysUnion<I>, InputTypeLabelsUnion<I>>
   public currentValue: ValidPortTypes[O] | undefined
 
-  private computeInputToOutput: (...args: InputDataTypes<I>) => ValidPortTypes[O]
+  private computeInputToOutput: (...args: InputTypes<I>) => ValidPortTypes[O]
 
   constructor(
-    computeInputToOutput: (...args: InputDataTypes<I>) => ValidPortTypes[O],
+    computeInputToOutput: (...args: InputTypes<I>) => ValidPortTypes[O],
     inputMap: I,
     public outputType: O,
     id: string = "default_node_id"
   ) {
     this.id = id
+    this.inputMap = inputMap
     this.computeInputToOutput = computeInputToOutput;
 
     this.inputStreams = new Map()
@@ -52,12 +114,12 @@ export class Node<I extends NodeInputs = any, O extends PortTypeKeys = any> {
     inputMap.forEach(([label, type]) => {
       const subject = new ReplaySubject<ValidPortTypes[PortTypeKeys]>(1)
       this.inputStreams.set(label, subject)
-      this.inputTypes.set(label, type)
+      this.inputTypes.set(label as InputKeysUnion<I>, type as InputTypeLabelsUnion<I>)
       inputSubjects.push(subject)
     })
 
     this.outputStream$ = combineLatest(inputSubjects).pipe(
-      map(inputs => this.computeInputToOutput(...inputs as unknown as InputDataTypes<I>)),
+      map(inputs => this.computeInputToOutput(...inputs as unknown as InputTypes<I>)),
       tap(output => this.currentValue = output)
     );
   }
@@ -66,8 +128,9 @@ export class Node<I extends NodeInputs = any, O extends PortTypeKeys = any> {
     return this.inputStreams.get(key)!
   }
 
-  getInputType(key: string): PortTypeKeys {
-    return this.inputTypes.get(key)!
+  getInputType<T extends I, K extends T[number][0]>(key: K): InputTypeByLabel<T, K> {
+    return this.inputMap.find(input => input[0] === key)
+    //return this.inputTypes.get(key)
   }
 }
 
