44cff89467b3f2ade6054c7437b9ef9ce85e3351
John Christensen <johnedchristensen@gmail.com>
2024-06-25 13:47:42 -0500

    Add color propogation and wire geometry debugging

--- src/basicGraph/WireBindingUtil.ts
+++ src/basicGraph/WireBindingUtil.ts
@@ -1,6 +1,8 @@
 import {
+  BindingOnCreateOptions,
   BindingOnDeleteOptions,
-  BindingUtil, TLBaseBinding
+  BindingOnShapeChangeOptions,
+  BindingUtil, TLBaseBinding,
 } from 'tldraw'
 
 
@@ -19,8 +21,56 @@ export class WireBindingUtil extends BindingUtil<WireBinding> {
       terminal: "start" as const
     }
   }
+
+  onAfterCreate(options: BindingOnCreateOptions<WireBinding>): void {
+    this.propogate(options.binding)
+  }
+
+  //start terminals need to propogate changes to wires
+  onAfterChangeToShape(options: BindingOnShapeChangeOptions<WireBinding>): void {
+    if (options.binding.props.terminal == "start") {
+      this.propogate(options.binding)
+    }
+  }
+
+  //end terminals propogate changes from wires to bound shapes
+  onAfterChangeFromShape(options: BindingOnShapeChangeOptions<WireBinding>): void {
+    if (options.binding.props.terminal == "end") {
+      this.propogate(options.binding)
+    }
+  }
+
+  private propogate(binding: WireBinding) {
+    const { parentShape, childShape } = this.getParentChild(binding)
+
+    if (!("color" in parentShape.props && "color" in childShape.props)) {
+      return
+    }
+    const newColor = parentShape.props.color
+    const oldColor = childShape.props.color
+
+    if (newColor == oldColor) {
+      return
+    }
+
+    console.log(`updating color from ${childShape.props.color} to `, newColor)
+    this.editor.updateShape({ id: childShape.id, type: childShape.type, props: { ...childShape.props, color: newColor } })
+  }
+
+  private getParentChild(binding: WireBinding) {
+    const { parentId, childId } = {
+      parentId: binding.props.terminal == "start" ? binding.toId : binding.fromId,
+      childId: binding.props.terminal == "start" ? binding.fromId : binding.toId
+    }
+    return {
+      parentShape: this.editor.getShape(parentId)!,
+      childShape: this.editor.getShape(childId)!
+    }
+
+  }
   //cleanup wire shape when binding is deleted.
   onAfterDelete(options: BindingOnDeleteOptions<WireBinding>): void {
     this.editor.deleteShape(options.binding.fromId)
   }
+
 }
--- src/basicGraph/WireShapeUtil.tsx
+++ src/basicGraph/WireShapeUtil.tsx
@@ -2,6 +2,7 @@ import {
   DefaultColorStyle,
   Mat,
   Polyline2d, RecordPropsType, SVGContainer, ShapeUtil, T, TLBaseShape,
+  TLDefaultColorStyle,
   TLOnResizeEndHandler,
   TLOnResizeHandler,
   TLOnResizeStartHandler,
@@ -11,8 +12,10 @@ import {
   TLOnTranslateEndHandler,
   TLOnTranslateHandler,
   TLOnTranslateStartHandler,
-  Vec, VecModel,
-  getDefaultColorTheme, useIsDarkMode,
+  Vec,
+  getDefaultColorTheme,
+  useEditor,
+  useIsDarkMode,
   vecModelValidator
 } from 'tldraw'
 import { WireBinding } from './WireBindingUtil'
@@ -39,7 +42,7 @@ export class WireShapeUtil extends ShapeUtil<WireShape> {
       color: 'black' as const,
       start: { x: 0, y: 0 },
       end: { x: 50, y: 100 },
-      isPlacing: true
+      isPlacing: true,
     }
   }
 
@@ -73,57 +76,57 @@ export class WireShapeUtil extends ShapeUtil<WireShape> {
 
 
   override getGeometry(shape: WireShape) {
-    // const points = [Vec.From(shape.props.start)
-    //   , Vec.From(shape.props.end)]
     const { start, end } = this.getLineShape(shape)
+
     return new Polyline2d({ points: [Vec.From(start), Vec.From(end)] })
   }
 
   override component(shape: WireShape) {
-    const isDarkMode = useIsDarkMode()
-    const theme = getDefaultColorTheme({ isDarkMode: isDarkMode })
+    const editor = useEditor()
     const { start, end } = this.getLineShape(shape)
 
-
-    return <LineComponent color={theme.black.solid}
+    return <LineComponent color={shape.props.color}
       shape={shape}
-      end={start}
-      start={end}
+      end={end}
+      start={start}
       strokeWidth={3}
+      debug={true}
+      isSelected={editor.getSelectedShapeIds()
+        .indexOf(shape.id) != -1
+      }
     />
   }
 
   override indicator(shape: WireShape) {
-    const isDarkMode = useIsDarkMode()
-    const theme = getDefaultColorTheme({ isDarkMode: isDarkMode })
     const { start, end } = this.getLineShape(shape)
 
-    return <LineComponent color={theme.blue.solid}
+    return <LineComponent color={"blue"}
       shape={shape}
       end={end}
       start={start}
       strokeWidth={1}
-    />
+      debug={false} />
   }
 
   /**
    * Calcualte a start and end wire position using the shapes bound to the wire
    */
-  getLineShape(shape: WireShape) {
+  getLineShape(shape: WireShape): { start: Vec, end: Vec } {
     const bindings = this.editor.getBindingsFromShape<WireBinding>(shape, "wire")
     const startBinding = bindings.find(b => b.props.terminal === "start")
+
     if (startBinding === undefined) {
       console.log("rendering line without start!")
       console.log(bindings)
       return {
-        start: { x: 0, y: 0 },
-        end: { x: 0, y: 0 }
+        start: Vec.From({ x: 0, y: 0 }),
+        end: Vec.From({ x: 0, y: 0 })
       }
     }
 
     const start = this.getTerminalInWireSpace(shape, startBinding)
-
     const endBinding = bindings.find(b => b.props.terminal === "end")
+
     if (endBinding === undefined) {
       // If there isn't an end yet, use current mouse position as the end point
       return {
@@ -134,7 +137,6 @@ export class WireShapeUtil extends ShapeUtil<WireShape> {
 
     const end = this.getTerminalInWireSpace(shape, endBinding)
     return { start: start, end: end }
-
   }
 
   /**
@@ -148,8 +150,14 @@ export class WireShapeUtil extends ShapeUtil<WireShape> {
     )
 
     const arrowPageTransform = this.editor.getShapePageTransform(wireShape)!
-    const pagePoint = Mat.applyToPoint(this.editor.getShapePageTransform(binding.toId)!, shapePoint)
-    const linePoint = Mat.applyToPoint(Mat.Inverse(arrowPageTransform), pagePoint)
+    const pagePoint = Mat.applyToPoint(
+      this.editor.getShapePageTransform(binding.toId)!,
+      shapePoint
+    )
+    const linePoint = Mat.applyToPoint(
+      Mat.Inverse(arrowPageTransform),
+      pagePoint
+    )
 
     return linePoint
   }
@@ -157,29 +165,57 @@ export class WireShapeUtil extends ShapeUtil<WireShape> {
 
 
 type LineProps = {
-  start: VecModel,
-  end: VecModel,
+  start: Vec,
+  end: Vec,
   strokeWidth: number,
-  color: string,
-  shape: WireShape
+  color: TLDefaultColorStyle,
+  shape: WireShape,
+  debug?: boolean
+  isSelected?: boolean
 }
 
-function LineComponent({ shape, start, end, color, strokeWidth }: LineProps) {
+function LineComponent({ shape, start, end, color, strokeWidth, debug = false, isSelected = false }: LineProps) {
+  const isDarkMode = useIsDarkMode()
+  const theme = getDefaultColorTheme({ isDarkMode: isDarkMode })
+  const colorVal = theme[color].solid
+
+  const startToEnd = Vec.Sub(end, start).toJson()
+  const center = Vec.Div(startToEnd, 2).add(start).toJson()
+  const arrowLegUnit = Vec.Uni(startToEnd).mul(10).toJson()
+  const radians = Math.PI * (3 / 4)
+  const arrowLeg1 = Vec.Rot(arrowLegUnit, radians).add(center).toJson()
+  const arrowLeg2 = Vec.Rot(arrowLegUnit, -radians).add(center).toJson()
+
   return (
     <SVGContainer id={shape.id} style={{ pointerEvents: "none" }}>
-      <g stroke={color}
+      <g stroke={colorVal}
         fill="none"
         strokeWidth={strokeWidth}
         strokeLinejoin="round"
         strokeLinecap="round"
         pointerEvents="none"
       >
-        <path d={`M ${start.x} ${start.y} L${end.x} ${end.y}`} />
-
+        <path
+          d={`M ${start.x} ${start.y} L${end.x} ${end.y}`} />
+        <g strokeWidth={strokeWidth * .75} opacity={.1}>
+          <path id="dir_ind_1"
+            d={`M ${center.x} ${center.y} L${arrowLeg1.x} ${arrowLeg1.y}`} />
+          <path id="dir_ind_2"
+            d={`M ${center.x} ${center.y} L${arrowLeg2.x} ${arrowLeg2.y}`} />
+        </g>
+        {debug && isSelected ? <g strokeWidth={.5} opacity={.5}>
+          <path stroke="#00cccc" id="start"
+            d={`M 0 0 L ${start.x} ${start.y}`} />
+          <path stroke="#cccc00" id="end"
+            d={`M 0 0 L ${end.x} ${end.y}`} />
+          <path stroke="#44cc44" id="arrow_direction_indicator_1"
+            d={`M 0 0 L${center.x} ${center.y}`} />
+          <path stroke="#cc0000" id="dir_ind_1"
+            d={`M ${center.x} ${center.y} L${arrowLeg1.x} ${arrowLeg1.y}`} />
+          <path stroke="#5555ee" id="dir_ind_2"
+            d={`M ${center.x} ${center.y} L${arrowLeg2.x} ${arrowLeg2.y}`} />
+        </g> : ""}
       </g>
     </SVGContainer>
   )
-
 }
-
-// <LineShapeSvg shape={shape} />
