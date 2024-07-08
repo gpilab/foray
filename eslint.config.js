cf312a4890edd6beb8777131dc336ef4d6145356
John Christensen <johnedchristensen@gmail.com>
2024-07-08 14:16:19 -0500

    Update package config

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,10 @@
 // @ts-check
-
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
-  // ...tseslint.configs.stylisticTypeChecked,
   {
     languageOptions: {
       parserOptions: {
@@ -33,18 +31,3 @@ export default tseslint.config(
     },
   },
 );
-
-
-//     "@typescript-eslint/no-unused-vars": [
-//       "error",
-//       {
-//         "args": "all",
-//         "argsIgnorePattern": "^_",
-//         "caughtErrors": "all",
-//         "caughtErrorsIgnorePattern": "^_",
-//         "destructuredArrayIgnorePattern": "^_",
-//         "varsIgnorePattern": "^_",
-//         "ignoreRestSiblings": true
-//       }
-//     ]
-//   }
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/gpi-vs/",
   plugins: [react({
     fastRefresh: process.env.NODE_ENV !== 'test'
   })],
