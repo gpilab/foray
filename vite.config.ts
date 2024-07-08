a3d68666be59568d30e115606c6f08cfc8582ff9
John Christensen <johnedchristensen@gmail.com>
2024-07-08 14:22:13 -0500

    fix path typo

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/gpi-vs/",
+  base: "/gpi-v2/",
   plugins: [react({
     fastRefresh: process.env.NODE_ENV !== 'test'
   })],
