ee625a0ebfa2f2b50a2383a39b7728094e6ff758
John Christensen <johnedchristensen@gmail.com>
2024-10-01 17:41:26 -0500

    Graph propogation without unnecessary copies??

--- framework/src/main.rs
+++ framework/src/main.rs
@@ -1,16 +1,16 @@
-use std::{collections::HashMap, fmt::Debug, rc::Rc};
+use std::{cell::RefCell, fmt::Debug, rc::Rc};
 
-use petgraph::{graph::DiGraph, visit::Topo, Direction::Incoming};
+use petgraph::{algo::toposort, graph::DiGraph, visit::Topo};
 #[derive(Debug)]
 struct Port {
-    _source_label: String,
-    val: Option<Rc<i32>>,
+    source_label: String,
+    val: Option<Rc<RefCell<i32>>>,
     target_label: String,
 }
 impl Port {
     fn new(source_label: &str, target_label: &str) -> Self {
         Self {
-            _source_label: source_label.into(),
+            source_label: source_label.into(),
             val: None,
             target_label: target_label.into(),
         }
@@ -32,8 +32,8 @@ enum NodeType {
 
 struct Node {
     name: String,
-    inputs: HashMap<String, Option<Rc<i32>>>,
-    outputs: HashMap<String, Option<Rc<i32>>>,
+    inputs: Vec<String>,
+    outputs: Vec<String>,
     n_type: NodeType,
 }
 
@@ -42,8 +42,8 @@ impl Node {
         Node {
             name,
             n_type,
-            inputs: inputs.iter().map(|s| (s.clone(), None)).collect(),
-            outputs: outputs.iter().map(|s| (s.clone(), None)).collect(),
+            inputs,
+            outputs,
         }
     }
 }
@@ -73,6 +73,12 @@ fn main() {
         ["a", "b"].map(String::from).to_vec(),
         ["out"].map(String::from).to_vec(),
     );
+    let node5 = Node::new(
+        "n3".into(),
+        NodeType::Offset(3),
+        ["a"].map(String::from).to_vec(),
+        ["out"].map(String::from).to_vec(),
+    );
 
     let mut g = Graph {
         g: DiGraph::<Node, Port>::new(),
@@ -81,85 +87,89 @@ fn main() {
     let n2 = g.g.add_node(node2);
     let n3 = g.g.add_node(node3);
     let n4 = g.g.add_node(node4);
+    let n5 = g.g.add_node(node5);
     g.g.add_edge(n1, n2, Port::new("out", "a"));
     g.g.add_edge(n1, n3, Port::new("out", "a"));
     g.g.add_edge(n2, n4, Port::new("out", "a"));
     g.g.add_edge(n3, n4, Port::new("out", "b"));
+    g.g.add_edge(n4, n5, Port::new("out", "a"));
 
     let mut topo = Topo::new(&g.g);
     while let Some(nx) = topo.next(&g.g) {
-        //let in_edges = g.g.neighbors_directed(nx, petgraph::Direction::Incoming);
-        //edges.fold(HashMap::new(), |acc, e| );
-        //let mut edge_walker = in_edges.detach();
-        // use a walker -- a detached neighbors iterator
-        //
-        let mut edges = g.g.neighbors_directed(nx, Incoming).detach();
-        while let Some(edge) = edges.next_edge(&g.g) {
-            let (nw, ew) = g.g.index_twice_mut(nx, edge);
-            //PERF: could this clone be avoided using something like
-            // rfcell or COW or someting?
-            dbg!(&nw);
-            nw.inputs.insert(ew.target_label.clone(), ew.val.clone());
-            dbg!(&nw);
-        }
-        //
-        //
-
-        ////let node = g.g.node_weight_mut(nx).unwrap();
-        //while let Some(edge) = edge_walker.next_edge(&g.g) {
-        //    //// get parent value
-        //    let parent_index = g.g.edge_endpoints(edge).unwrap().0;
-        //    let parent = g.g.node_weight(parent_index).unwrap();
-        //    //let (parent, ewp) = g.g.index_twice_mut(parent_index, edge);
-        //    let (node, ew) = g.g.index_twice_mut(nx, edge);
-        //
-        //    let val = parent.outputs.get(&ew.0).unwrap();
-        //
-        //    //// copy value into input
-        //    node.inputs.insert(ew.1.clone(), *val);
-        //}
-
-        let node = g.g.node_weight_mut(nx).unwrap();
+        let node = g.g.node_weight(nx).unwrap();
         println!("start {:?}", node);
+        println!("{:?}", g);
 
         match node.n_type {
             NodeType::Const(val) => {
-                node.outputs.insert("out".into(), Some(val.into()));
+                let mut in_edges =
+                    g.g.neighbors_directed(nx, petgraph::Direction::Outgoing)
+                        .detach();
+                while let Some(edge) = in_edges.next_edge(&g.g) {
+                    let (_nw, ew) = g.g.index_twice_mut(nx, edge);
+                    ew.val = Some(Rc::new(val.into()));
+                }
             }
             NodeType::Add => {
-                let out = node.inputs.get("a").unwrap().as_ref().unwrap().as_ref()
-                    + node.inputs.get("b").unwrap().as_ref().unwrap().as_ref();
-                node.outputs.insert("out".into(), Some(out.into()));
+                let in_edge = g.g.edges_directed(nx, petgraph::Direction::Incoming);
+
+                let a = &in_edge
+                    .clone()
+                    .find(|e| e.weight().target_label == "a")
+                    .unwrap()
+                    .weight()
+                    .val;
+                let b = &in_edge
+                    .clone()
+                    .find(|e| e.weight().target_label == "b")
+                    .unwrap()
+                    .weight()
+                    .val;
+
+                fn my_add(a: &i32, b: &i32) -> i32 {
+                    a + b
+                }
+                let out = my_add(&a.as_ref().unwrap().borrow(), &b.as_ref().unwrap().borrow());
+
+                let mut in_edges =
+                    g.g.neighbors_directed(nx, petgraph::Direction::Outgoing)
+                        .detach();
+                while let Some(edge) = in_edges.next_edge(&g.g) {
+                    let (_nw, ew) = g.g.index_twice_mut(nx, edge);
+                    ew.val = Some(Rc::new(out.into()));
+                }
             }
             NodeType::_Sub => {}
             NodeType::Offset(val) => {
-                let out = node
-                    .inputs
-                    .get("a")
-                    .expect("input a should exist for Offset Nodes")
-                    .as_ref()
-                    .expect("Input should be populated")
-                    .as_ref()
-                    + val;
-                node.outputs.insert("out".into(), Some(out.into()));
+                let in_edge = g.g.edges_directed(nx, petgraph::Direction::Incoming);
+                dbg!(&nx);
+                dbg!(&in_edge);
+
+                let a = &in_edge
+                    .clone()
+                    .find(|e| dbg!(&e.weight().target_label) == "a")
+                    .unwrap()
+                    .weight()
+                    .val;
+
+                fn my_add(a: &i32, b: &i32) -> i32 {
+                    a + b
+                }
+
+                let out = my_add(&a.as_ref().unwrap().borrow(), &val);
+
+                let mut in_edges =
+                    g.g.neighbors_directed(nx, petgraph::Direction::Outgoing)
+                        .detach();
+                while let Some(edge) = in_edges.next_edge(&g.g) {
+                    let (_nw, ew) = g.g.index_twice_mut(nx, edge);
+                    ew.val = Some(Rc::new(out.into()));
+                }
             }
         };
-        println!("end {:?}", node);
-
-        let mut out_edges =
-            g.g.neighbors_directed(nx, petgraph::Direction::Outgoing)
-                .detach();
-        while let Some(edge) = out_edges.next_edge(&g.g) {
-            let (nw, ew) = g.g.index_twice_mut(nx, edge);
-            //PERF: could this clone be avoided using something like
-            // rfcell or COW or someting?
-            dbg!(&ew);
-            ew.val = nw.outputs.get(&ew._source_label).unwrap().clone();
-            dbg!(&ew);
-        }
     }
-    //println!("{:?}", &g);
-    //println!("{:?}", toposort(&g.g, None));
+    println!("{:?}", &g);
+    println!("{:?}", toposort(&g.g, None));
 
     // Output the tree to `graphviz` `DOT` format
     //println!("{:?}", Dot::with_config(&g.g, &[Config::EdgeNoLabel]));
