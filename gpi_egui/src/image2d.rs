474d5a6a357fc4a6379dd153c03e1f329c5d0ef3
John Christensen <jechristens3@wisc.edu>
2024-11-15 09:54:48 -0600

    Replace sliders with lerp histogram

--- gpi_egui/src/image2d.rs
+++ gpi_egui/src/image2d.rs
@@ -9,15 +9,41 @@ use ndarray_stats::{
     HistogramExt, QuantileExt,
 };
 
+#[derive(Clone)]
+pub struct ImageDisplayOptions {
+    pub window: f32,
+    pub level: f32,
+    pub floor: f32,
+    pub ceiling: f32,
+}
+
+impl ImageDisplayOptions {
+    pub fn apply_constraints(&mut self) {
+        let past = self.clone();
+        self.ceiling = past.ceiling.max(past.floor + 1.);
+        self.floor = past.floor.clamp(0., past.ceiling - 1.);
+    }
+}
+
+impl Default for ImageDisplayOptions {
+    fn default() -> Self {
+        Self {
+            window: 1.0,
+            level: 0.0,
+            floor: 0.0,
+            ceiling: 255.0,
+        }
+    }
+}
+
 pub struct Image2D {
     ///cached texture for rendering
     texture: Option<TextureHandle>,
+    //image_data: Option<ImageData>,
     base_array: Array2<u8>,
-    adjusted_image: ImageData,
+    pub histogram: Vec<usize>,
     width: usize,
     height: usize,
-    window: f64,
-    level: f64,
 }
 
 impl Default for Image2D {
@@ -26,23 +52,36 @@ impl Default for Image2D {
         let array = Array2::zeros(default_size);
         Self {
             texture: None,
+            //image_data: None,
             base_array: array.clone(),
-            adjusted_image: Self::array_to_image(array, default_size),
+            histogram: Self::image_distribution(&array),
+            //adjusted_image: Self::array_to_image(array, default_size),
             width: 300,
             height: 300,
-            window: 1.0,
-            level: 0.0,
         }
     }
 }
 
 impl Image2D {
-    pub fn get_handle(&mut self, ui: &Ui) -> &TextureHandle {
+    pub fn get_handle(&mut self, ui: &Ui, display_options: &ImageDisplayOptions) -> &TextureHandle {
+        // texture gets recalucated whenever it gets set to None
         self.texture.get_or_insert_with(|| {
-            // Load the texture only once.
+            let shape = self.base_array.shape();
+
+            let altered_quantized = self.base_array.map(|&v| {
+                Self::quantize(
+                    v as f32,
+                    display_options.floor,
+                    display_options.ceiling,
+                    display_options.window,
+                    display_options.level,
+                )
+            });
+
+            let image_data = Self::array_to_image(altered_quantized, (shape[0], shape[1]));
             ui.ctx().load_texture(
                 "my-image",
-                self.adjusted_image.clone(),
+                image_data,
                 eframe::egui::TextureOptions {
                     magnification: TextureFilter::Nearest,
                     ..Default::default()
@@ -50,56 +89,44 @@ impl Image2D {
             )
         })
     }
-    pub fn from_array2d(array2d: ndarray::Array2<f64>, window: f64, level: f64) -> Self {
-        let max = *array2d.max().unwrap();
-        let min = *array2d.min().unwrap();
+    pub fn from_array2d(array2d: ndarray::Array2<f64>) -> Self {
+        let max = *array2d.max().unwrap() as f32;
+        let min = *array2d.min().unwrap() as f32;
         let shape = array2d.shape();
         let width = shape[1];
         let height = shape[0];
 
-        let base_array = array2d.map(|&v| Self::quantize(v, min, max, 1., 0.));
-
-        let altered_quantized =
-            base_array.map(|&v| Self::quantize(v as f64, 0.0, 255.0, window, level));
+        let base_array = array2d.map(|&v| Self::quantize(v as f32, min, max, 1., 0.));
 
         Self {
             texture: None,
+            histogram: Self::image_distribution(&base_array),
             base_array,
-            adjusted_image: Self::array_to_image(altered_quantized, (shape[0], shape[1])),
             width,
             height,
-            window,
-            level,
         }
     }
+
     /// Create a 2d image from a 3d image by tileing the 2d image
-    ///
-    pub fn from_array3d(
-        array3d: ndarray::Array3<f64>,
-        n_rows: usize,
-        n_columns: usize,
-        window: f64,
-        level: f64,
-    ) -> Self {
+    pub fn from_array3d(array3d: ndarray::Array3<f64>, n_rows: usize, n_columns: usize) -> Self {
         let shape = array3d.raw_dim();
         let num_slices = shape[0];
         let slice_height = shape[1];
         let slice_width = shape[2];
 
-        println!("w: {slice_width}, h: {slice_height}, z: {num_slices}");
+        //TODO: support non filled grids,
+        //handle failure gracefully
+        assert!(n_rows * n_columns == num_slices);
 
         let tiled_height = slice_height * n_rows;
         let tiled_width = slice_width * n_columns;
 
+        // Allocate the final image size
         let mut out_image: Array2<f64> = Array2::zeros((tiled_height, tiled_width));
 
-        //let mut windows =
-        //    mut_view.windows_with_stride((slice_height, slice_width), (slice_height, slice_width));
-
-        //windows.into_iter().enumerate().for_each(|(i, w)| {
-        //    let mut_w = w.slice_mut();
-        //    view_mut().assign(&array3d.slice(s![i, .., ..]))
-        //});
+        // Loop over the coordinates of the top left image of
+        // each desired image tile, and copy the image  from the input array
+        // into the correct location in the 2d image
         for n_y in 0..n_rows {
             for n_x in 0..n_columns {
                 let z = n_x + n_y * n_columns;
@@ -112,68 +139,23 @@ impl Image2D {
                     .assign(&slice);
             }
         }
-        //let array2d = array3d
-        //    .into_shape_with_order((tiled_height, tiled_width))
-        //    .unwrap();
 
-        let f_shape = out_image.raw_dim();
-        let final_height = f_shape[0];
-        let final_width = f_shape[1];
-
-        println!("final w: {final_width}, final h: {final_height}");
-
-        //let array2d = array3d.slice(s![1, .., ..]).to_owned();
-        Self::from_array2d(out_image, window, level)
+        Self::from_array2d(out_image)
     }
 
-    pub fn from_path(path: &std::path::Path, window: f64, level: f64) -> Self {
+    pub fn from_path(path: &std::path::Path) -> Self {
         let image = ImageReader::open(path).unwrap().decode().unwrap();
         let size = (image.width() as usize, image.height() as usize);
         let image_vec = image.to_luma8().iter().map(|&b| b as f64).collect();
         let array2d = Array::from_shape_vec(size, image_vec).unwrap();
-        Self::from_array2d(array2d, window, level)
+        Self::from_array2d(array2d)
     }
 
-    pub fn update_levels(&mut self, window: f64, level: f64) {
-        self.window = window;
-        self.level = level;
-        let shape = self.base_array.shape();
-        let altered_quantized = self
-            .base_array
-            .map(|&v| Self::quantize(v as f64, 0.0, 255.0, window, level));
-        self.adjusted_image = Self::array_to_image(altered_quantized, (shape[0], shape[1]));
+    /// Flag the image as needing to recompute it's texture
+    pub fn request_redraw(&mut self) {
         self.texture = None;
     }
-    //// PERF: Compute the histogram once, and store in self
-    pub fn image_distribution(&self) -> Vec<usize> {
-        let grid = Grid::from(vec![Bins::new(Edges::from((0..=255).collect::<Vec<_>>()))]); //let grid = GridBuilder::<u8>::from_array(&self.base_array)
-                                                                                            //    .build();
-        let size = self.base_array.len();
-        let histogram = self
-            .base_array
-            .clone()
-            .into_shape_with_order((size, 1))
-            .unwrap()
-            .histogram(grid);
 
-        //// remove background
-        let max_i = histogram.counts().argmax().unwrap()[0];
-        let mut counts = histogram.counts().to_owned().into_raw_vec_and_offset().0;
-
-        let left = match max_i {
-            0 => 0,
-            1..255 => counts[max_i],
-            _ => panic!("histogram  unexpectedly has more than 255 values"),
-        };
-        let right = match max_i {
-            254 => 0,
-            0..254 => counts[max_i],
-            _ => panic!("histogram  unexpectedly has more than 255 values"),
-        };
-        let averaged = (left + right) / 2;
-        counts[max_i] = averaged;
-        counts
-    }
     /// width / height
     pub fn aspect_ratio(&self) -> f64 {
         self.width as f64 / self.height as f64
@@ -182,7 +164,7 @@ impl Image2D {
 
 // Helpers
 impl Image2D {
-    fn quantize(value: f64, min: f64, max: f64, window: f64, level: f64) -> u8 {
+    fn quantize(value: f32, min: f32, max: f32, window: f32, level: f32) -> u8 {
         let normalized = 255.0 * inverse_lerp(min..=max, value).unwrap_or(0.0);
         let windowed = normalized * window + level;
         windowed.round() as u8
@@ -197,5 +179,35 @@ impl Image2D {
         //let image_buffer = image.to_luma8();
         let pixels = image_buffer.as_flat_samples();
         ColorImage::from_gray([size.1, size.0], pixels.as_slice()).into()
+    }
+
+    //// PERF: Compute the histogram once, and store in self
+    pub fn image_distribution(array2d: &Array2<u8>) -> Vec<usize> {
+        let grid = Grid::from(vec![Bins::new(Edges::from((0..=255).collect::<Vec<_>>()))]);
+
+        let size = array2d.len();
+        let histogram = array2d
+            .clone()
+            .into_shape_with_order((size, 1))
+            .unwrap()
+            .histogram(grid);
+
+        //// remove background
+        let max_i = histogram.counts().argmax().unwrap()[0];
+        let mut counts = histogram.counts().to_owned().into_raw_vec_and_offset().0;
+
+        //let left = match max_i {
+        //    0 => 0,
+        //    1..255 => counts[max_i],
+        //    _ => panic!("histogram  unexpectedly has more than 255 values"),
+        //};
+        //let right = match max_i {
+        //    254 => 0,
+        //    0..254 => counts[max_i],
+        //    _ => panic!("histogram  unexpectedly has more than 255 values"),
+        //};
+        //let averaged = (left + right) / 2;
+        counts[max_i] = 0;
+        counts
     }
 }
--- gpi_egui/src/main.rs
+++ gpi_egui/src/main.rs
@@ -1,10 +1,12 @@
-use egui_plot::{Bar, BarChart, Plot};
 use ndarray::Array3;
 use ndarray_npy::ReadNpyExt;
 use std::{env, fs::File, path::Path};
 
-use eframe::egui::{self, Color32, Stroke};
-use gpi_egui::image2d::Image2D;
+use eframe::{
+    egui::{self, Color32, Id, Painter, Pos2, Rect, Sense, Shape, Stroke, Ui, Vec2},
+    emath::{self, RectTransform},
+};
+use gpi_egui::image2d::{Image2D, ImageDisplayOptions};
 
 fn main() {
     let mut native_options = eframe::NativeOptions::default();
@@ -18,8 +20,7 @@ fn main() {
 
 struct MyEguiApp {
     image2d: Image2D,
-    window: f64,
-    level: f64,
+    image_display_options: ImageDisplayOptions,
 }
 
 impl MyEguiApp {
@@ -38,14 +39,11 @@ impl MyEguiApp {
         let reader = File::open(path).unwrap();
         let test_image = Array3::<f64>::read_npy(reader).unwrap();
 
-        let window = 1.;
-        let level = 0.;
-        let test_slice = Image2D::from_array3d(test_image, 6, 5, window, level);
+        let test_slice = Image2D::from_array3d(test_image, 6, 5);
         Self {
             //image2d: Image2D::from_array2d(test_image),
             image2d: test_slice,
-            window,
-            level,
+            image_display_options: Default::default(),
         }
     }
 }
@@ -54,7 +52,7 @@ impl eframe::App for MyEguiApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
             let aspect = self.image2d.aspect_ratio() as f32;
-            let texture = self.image2d.get_handle(ui);
+            let texture = self.image2d.get_handle(ui, &self.image_display_options);
 
             let space = ui.available_size();
             let ui_width = space[0];
@@ -66,61 +64,24 @@ impl eframe::App for MyEguiApp {
                 (ui_height * aspect, ui_height)
             };
 
+            let image_width = image_width.max(1.);
+            let image_height = image_height.max(1.);
+
             //// Image
             ui.image((texture.id(), [image_width, image_height].into()));
 
             //// HISTOGRAM
-            let histogram = self.image2d.image_distribution();
-            let bars = histogram
-                .into_iter()
-                .enumerate()
-                .map(|(i, x)| {
-                    Bar::new(i as f64, x as f64)
-                        .stroke(Stroke {
-                            width: 0.01,
-                            color: Color32::WHITE,
-                        })
-                        .fill(Color32::WHITE)
-                        .width(0.8)
-                })
-                .collect();
-            let bar_chart: BarChart = BarChart::new(bars);
-            egui::Frame::default()
-                .inner_margin(1.0)
-                .fill(egui::Color32::GRAY)
-                .show(ui, |ui| {
-                    egui::Frame::default()
-                        .inner_margin(1.0)
-                        .fill(egui::Color32::BLACK)
-                        .show(ui, |ui| {
-                            Plot::new("my_plot")
-                                .height(100.)
-                                .allow_zoom(false)
-                                .allow_scroll(false)
-                                .allow_drag(false)
-                                .show_x(false)
-                                .show_y(false)
-                                .clamp_grid(true)
-                                .show_background(false)
-                                .show_axes(false)
-                                .show_grid(false)
-                                //.x_grid_spacer(|_grid_input| {
-                                //    vec![GridMark {
-                                //        value: 0.0,
-                                //        step_size: 10.0,
-                                //    }]
-                                //})
-                                //.y_grid_spacer(|_grid_input| {
-                                //    vec![GridMark {
-                                //        value: 0.0,
-                                //        step_size: 10.0,
-                                //    }]
-                                //})
-                                .show(ui, |plot_ui| plot_ui.bar_chart(bar_chart));
-                        });
-                });
+            if draw_histogram(
+                ui,
+                &self.image2d.histogram,
+                &mut self.image_display_options.floor,
+                &mut self.image_display_options.ceiling,
+            ) {
+                self.image2d.request_redraw();
+            }
 
             //// Sliders
+
             egui::Grid::new("my_grid")
                 .num_columns(2)
                 .spacing([20.0, 4.0])
@@ -130,18 +91,215 @@ impl eframe::App for MyEguiApp {
                     const SLIDER_MARGIN: f32 = 100.;
                     ui.style_mut().spacing.slider_width =
                         (ui.available_width() - SLIDER_MARGIN).max(1.0);
-                    let window_slider = ui.add(egui::Slider::new(&mut self.window, 0.0..=5.0));
+
+                    let window_slider = ui.add(egui::Slider::new(
+                        &mut self.image_display_options.window,
+                        0.0..=5.0,
+                    ));
                     ui.end_row();
 
                     ui.label("Level");
-                    let level_slider = ui.add(egui::Slider::new(&mut self.level, -255.0..=255.0));
+                    let level_slider = ui.add(egui::Slider::new(
+                        &mut self.image_display_options.level,
+                        -255.0..=255.0,
+                    ));
                     ui.end_row();
 
+                    ui.label("Floor");
+                    let floor_slider = ui.add(egui::Slider::new(
+                        &mut self.image_display_options.floor,
+                        0.0..=255.0,
+                    ));
+                    ui.end_row();
+
+                    ui.label("Ceiling");
+                    let ceiling_slider = ui.add(egui::Slider::new(
+                        &mut self.image_display_options.ceiling,
+                        0.0..=255.0,
+                    ));
+                    ui.end_row();
+
+                    //// Constraints
+                    self.image_display_options.apply_constraints();
+
                     //// Update Image with new values
-                    if window_slider.changed() || level_slider.changed() {
-                        self.image2d.update_levels(self.window, self.level);
+                    if window_slider.changed()
+                        || level_slider.changed()
+                        || floor_slider.changed()
+                        || ceiling_slider.changed()
+                    {
+                        self.image2d.request_redraw();
                     }
                 });
         });
     }
+}
+
+//fn draw_histogram(
+//    ui: &mut egui::Ui,
+//    histogram: &[usize],
+//    size: (f32, f32),
+//    image_display_options: &mut ImageDisplayOptions,
+//)-> {
+//    lerp_ui(
+//        ui,
+//        &histogram,
+//        &mut image_display_options.floor,
+//        &mut image_display_options.ceiling,
+//    )
+
+//let max = (1. + *histogram.iter().max().unwrap() as f64).log10();
+//let left = image_display_options.floor as f64;
+//let right = image_display_options.ceiling as f64;
+//let line_min = 1.0;
+//let line_max = (max - 1.0) as f64;
+//let lines = Line::new(vec![
+//    [0.0, line_min],
+//    [left, line_min],
+//    [right, line_max],
+//    [255.0, line_max],
+//])
+//.width(2.0);
+//
+//let bars = histogram
+//    .into_iter()
+//    .enumerate()
+//    .map(|(i, &x)| {
+//        Bar::new(i as f64, (1. + x as f64).log10())
+//            .stroke(Stroke {
+//                width: 0.01,
+//                color: Color32::WHITE,
+//            })
+//            .fill(Color32::WHITE)
+//            .width(0.8)
+//    })
+//    .collect();
+//let bar_chart: BarChart = BarChart::new(bars);
+//egui::Frame::default()
+//    //.inner_margin(1.0)
+//    //.fill(egui::Color32::GRAY)
+//    //.show(ui, |ui| {
+//    //    egui::Frame::default()
+//    //        .inner_margin(1.0)
+//    //        .fill(egui::Color32::BLACK)
+//    .show(ui, |ui| {
+//        Plot::new("my_plot")
+//            .width(size.0)
+//            .height(size.1)
+//            .allow_zoom(false)
+//            .allow_scroll(false)
+//            .allow_drag(false)
+//            .show_x(false)
+//            .show_y(false)
+//            .clamp_grid(true)
+//            .show_background(false)
+//            .show_axes(false)
+//            .show_grid(false)
+//            .set_margin_fraction((0., 0.).into())
+//            //.x_grid_spacer(|_grid_input| {
+//            //    vec![GridMark {
+//            //        value: 0.0,
+//            //        step_size: 10.0,
+//            //    }]
+//            //})
+//            //.y_grid_spacer(|_grid_input| {
+//            //    vec![GridMark {
+//            //        value: 0.0,
+//            //        step_size: 10.0,
+//            //    }]
+//            //})
+//            .show(ui, |plot_ui| {
+//                plot_ui.bar_chart(bar_chart);
+//                plot_ui.line(lines)
+//            });
+//        //        });
+//    });
+//}
+
+fn draw_histogram(ui: &mut Ui, histogram: &[usize], floor: &mut f32, max: &mut f32) -> bool {
+    let width = ui.available_width();
+    let height = 200.0;
+    let (response, painter) = ui.allocate_painter(Vec2::new(width, height), Sense::hover());
+
+    let to_screen = emath::RectTransform::from_to(
+        Rect::from_min_size(Pos2::ZERO, response.rect.size()),
+        response.rect,
+    );
+
+    //// Histogram
+    let histogram_max = *histogram.iter().max().unwrap_or(&1);
+    histogram
+        .iter()
+        .map(|&c| (c as f32 / histogram_max as f32) * height)
+        .enumerate()
+        .map(|(i, y)| Pos2::new(i as f32 / 255. * width, y))
+        .map(|p| [p, Pos2 { x: p.x, y: 0.0 }])
+        .map(|l| [to_screen.transform_pos(l[0]), to_screen.transform_pos(l[1])])
+        .for_each(|l| {
+            painter.add(Shape::line_segment(
+                l,
+                Stroke {
+                    width: 1.0,
+                    color: Color32::WHITE,
+                },
+            ));
+        });
+
+    //// Lerp Layer
+    let line_inset = 10.0;
+    let p0 = (0., height - line_inset).into();
+    let p1 = (*floor, height - line_inset).into();
+    let p2 = (*max, line_inset).into();
+    let p3 = (width, line_inset).into();
+    let point_id_1 = response.id.with(1);
+    let point_id_2 = response.id.with(2);
+
+    painter.add(Shape::line(
+        [p0, p1, p2, p3]
+            .iter()
+            .map(|p| to_screen.transform_pos(*p))
+            .collect(),
+        Stroke {
+            color: Color32::WHITE,
+            width: 2.0,
+        },
+    ));
+
+    let mut request_redraw = false;
+
+    if let Some(d) = draw_drag_point(ui, &painter, point_id_1, p1, to_screen) {
+        *floor += d.x;
+        request_redraw = true;
+    };
+    if let Some(d) = draw_drag_point(ui, &painter, point_id_2, p2, to_screen) {
+        *max += d.x;
+        request_redraw = true;
+    };
+
+    request_redraw
+}
+
+fn draw_drag_point(
+    ui: &mut Ui,
+    painter: &Painter,
+    id: Id,
+    pos: Pos2,
+    to_screen: RectTransform,
+) -> Option<Vec2> {
+    let point_in_screen = to_screen.transform_pos(pos);
+    let point_rect = Rect::from_center_size(point_in_screen, (20., 20.).into());
+
+    let point_response = ui.interact(point_rect, id, Sense::drag());
+
+    let point1 = to_screen.from().clamp(pos);
+
+    let point_in_screen = to_screen.transform_pos(point1);
+    //let stroke = ui.style().interact(&point_response).fg_stroke;
+
+    let shape = Shape::circle_filled(point_in_screen, 10., Color32::GRAY);
+
+    //to_screen * point1;
+
+    painter.add(shape);
+    (point_response.drag_delta().length_sq() > 0.).then(|| point_response.drag_delta())
 }
