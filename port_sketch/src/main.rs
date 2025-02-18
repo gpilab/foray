5246af582fa67e816109a84b60582ae4842c67ab
John Christensen <jechristens3@wisc.edu>
2025-02-18 15:52:26 -0600

    Some kind of nested types

--- port_sketch/src/main.rs
+++ port_sketch/src/main.rs
@@ -8,13 +8,15 @@ trait Primitive {}
 impl Primitive for u32 {}
 impl Primitive for f32 {}
 impl Primitive for Box<dyn Primitive> {}
-impl Primitive for Box<u32> {}
-impl Primitive for Box<f32> {}
+impl<T> Primitive for Box<T> {}
+impl<T: Primitive> Primitive for Port<T> {}
+//impl Primitive for Box<u32> {}
+//impl Primitive for Box<f32> {}
 //impl Primitive for Port<Box<dyn Primitive>> {}
-impl Primitive for Port<u32> {}
-impl Primitive for Port<f32> {}
-impl Primitive for Port<Box<u32>> {}
-impl Primitive for Port<Box<f32>> {}
+//impl Primitive for Port<u32> {}
+//impl Primitive for Port<f32> {}
+//impl Primitive for Port<Box<u32>> {}
+//impl Primitive for Port<Box<f32>> {}
 
 enum Port<T>
 where
@@ -38,12 +40,29 @@ fn main() {
 
     let ex: Box<dyn Primitive> = Box::new(2u32);
     let exf: Box<dyn Primitive> = Box::new(2.0f32);
+    let my_struct: Port<u32> = Port::Struct([("foo".to_string(), ex)].into());
+    let my_struct2 = Port::Struct([("bar".to_string(), exf)].into());
+
+    let ex: Box<dyn Primitive> = Box::new(2u32);
+    let exf: Box<dyn Primitive> = Box::new(2.0f32);
 
     let d: Port<Box<dyn Primitive>> = Port::Struct(
         [
             ("foo".to_string(), ex),
             ("bar".to_string(), exf),
-            ("baz".to_string(), Box::new(a)),
+            ("baz1".to_string(), Box::new(a)),
+            ("baz1".to_string(), Box::new(b1)),
+            ("baz2".to_string(), Box::new(5)),
+            ("baz3".to_string(), Box::new(5.0)),
+            ("baz4".to_string(), Box::new(c)),
+            (
+                "baz5".to_string(),
+                Box::new(Port::Array(Shape(vec![1, 2]), vec![1, 2])),
+            ),
+            (
+                "baz6".to_string(),
+                Box::new(Port::Array(Shape(vec![1, 2]), vec![my_struct, my_struct2])),
+            ),
         ]
         .into(),
     );
